7d57ceda448de23f63641fa375bdfb581fcaf6c8
Anton Evangelatov <anton.evangelatov@gmail.com>
2020-08-18 18:36:34 +0200

    update ./diff.sh and Makefile to use vars

--- Makefile
+++ Makefile
@@ -7,8 +7,8 @@ GENCOMMIT = `git rev-list -1 HEAD`
 gen:
 	find gen/suites -maxdepth 1 -mindepth 1 -type d -print0 | xargs -I '{}' -n1 -0 bash -c 'dir="$$(basename {})" && echo "=== $${dir} ===" && cd {} && go run -ldflags "-X github.com/filecoin-project/test-vectors/gen/builders.GenscriptCommit=${GENCOMMIT}" . -o "../../../corpus/$${dir}"'
 
-compare-with-lotus-next:
-	pushd gen && go get github.com/filecoin-project/specs-actors@master && go get github.com/filecoin-project/lotus@a0c0d9c98aae && popd && cp -R corpus corpus-current && make gen && mv corpus corpus-new && ./diff.sh
+compare-corpus:
+	./diff.sh ${REPO} ${BRANCH}
 
 validate:
 	cd ./validate && go run ./main.go
--- diff.sh
+++ diff.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+set -o errexit
+set -o pipefail
+set -e
+
+REPO=$1
+BRANCH=$2
+
+# Validate required arguments
+if [ -z "$REPO" ]; then
+  echo -e "Please provider REPO input variable. For example: \`./diff.sh lotus next\`"
+  exit 2
+fi
+if [ -z "$BRANCH" ]; then
+  echo -e "Please provider BRANCH input variable. For example: \`./diff.sh lotus next\`"
+  exit 2
+fi
+
+pushd gen
+go get github.com/filecoin-project/${REPO}@${BRANCH}
+popd
+
+tmp_dir=$(mktemp -d -t ci-compare-corpus-XXXX)
+echo "temporary dir: $tmp_dir"
+
+cp -R corpus corpus-current
+cp -R corpus-current $tmp_dir/corpus-current
+make gen
+mv corpus $tmp_dir/corpus-new
+mv corpus-current corpus
+
+pushd $tmp_dir
+
 # Update in-place all newly generated JSON corpus files and remove the "_meta" field.
 for f in $(find ./corpus-new -type f); do
   echo "$(jq 'del(._meta)' $f)" > $f;
@@ -12,3 +44,7 @@ done
 
 # Run diff between the old and the new test vectors
 diff -qr corpus-new/ corpus-current/
+
+popd
+
+rm -rf $tmp_dir
