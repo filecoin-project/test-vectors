d255b2c193c789dcb5f3c5ab1d8bf334ccf11391
Raúl Kripalani <raul@protocol.ai>
2020-08-19 14:34:26 +0100

    start vm_violations suite.

---
+++ gen/suites/vm_violations/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/filecoin-project/specs-actors/actors/runtime/exitcode"
+
+	"github.com/filecoin-project/test-vectors/chaos"
+	. "github.com/filecoin-project/test-vectors/gen/builders"
+)
+
+func main() {
+	g := NewGenerator()
+	defer g.Wait()
+
+	g.MessageVectorGroup("caller_validation",
+		&MessageVectorGenItem{
+			Metadata: &Metadata{
+				ID:      "none",
+				Version: "v1",
+				Desc:    "verifies that an actor that performs no caller validation fails",
+			},
+			Selector: "chaos_actor=true",
+			Func:     callerValidation(&chaos.CallerValidationBranchNone, exitcode.SysErrorIllegalActor),
+		},
+		&MessageVectorGenItem{
+			Metadata: &Metadata{
+				ID:      "twice",
+				Version: "v1",
+				Desc:    "verifies that an actor that validates the caller twice fails",
+			},
+			Selector: "chaos_actor=true",
+			Func:     callerValidation(&chaos.CallerValidationBranchTwice, exitcode.SysErrorIllegalActor),
+		},
+		&MessageVectorGenItem{
+			Metadata: &Metadata{
+				ID:      "nil-allowed-address-set",
+				Version: "v1",
+				Desc:    "verifies that an actor that validates against a nil allowed address set fails",
+			},
+			Selector: "chaos_actor=true",
+			Func:     callerValidation(&chaos.CallerValidationBranchImmediateCallerAddrNoArgs, exitcode.SysErrForbidden),
+		},
+		&MessageVectorGenItem{
+			Metadata: &Metadata{
+				ID:      "nil-allowed-type-set",
+				Version: "v1",
+				Desc:    "verifies that an actor that validates against a nil allowed type set fails",
+			},
+			Selector: "chaos_actor=true",
+			Func:     callerValidation(&chaos.CallerValidationBranchImmediateCallerTypeNoArgs, exitcode.SysErrForbidden),
+		},
+	)
+}
