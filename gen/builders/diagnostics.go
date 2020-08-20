59a7a8ff527ae48da1591360574f9e0e4431aec5
Will <willscott@gmail.com>
2020-08-20 05:40:22 -0700

    strip times from traces (#46)
    Co-authored-by: Raúl Kripalani <raul@protocol.ai>

--- gen/builders/diagnostics.go
+++ gen/builders/diagnostics.go
@@ -5,6 +5,7 @@ import (
 	"compress/gzip"
 	"encoding/base64"
 	"encoding/json"
+	"time"
 
 	"github.com/filecoin-project/lotus/chain/types"
 
@@ -17,7 +18,7 @@ const LotusExecutionTraceV1 = "Lotus-ExecutionTrace-V1"
 // to the test vector serialized diagnostic format.
 func EncodeTraces(traces []types.ExecutionTrace) *schema.Diagnostics {
 	d := schema.Diagnostics{Format: LotusExecutionTraceV1}
-	serialized, err := json.Marshal(traces)
+	serialized, err := json.Marshal(cleanTraces(traces))
 	if err != nil {
 		panic(err)
 	}
@@ -35,4 +36,17 @@ func EncodeTraces(traces []types.ExecutionTrace) *schema.Diagnostics {
 
 	d.Data = data.Bytes()
 	return &d
+}
+
+// cleanTraces recursively strips variable/volatile fields from execution traces,
+// e.g. TimeTaken, in order to remove noise and facilitate comparison and diffing.
+func cleanTraces(t []types.ExecutionTrace) []types.ExecutionTrace {
+	for _, e := range t {
+		e.Duration = time.Duration(0)
+		e.Subcalls = cleanTraces(e.Subcalls)
+		for _, g := range e.GasCharges {
+			g.TimeTaken = time.Duration(0)
+		}
+	}
+	return t
 }
