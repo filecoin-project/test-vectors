5cdfd8df090dbe614adfa2a50165aa2f6220c87c
Alan Shaw <alan@tableflip.io>
2020-08-24 11:09:26 +0100

    fix: verify sent funds

--- gen/suites/transfer/system_receiver.go
+++ gen/suites/transfer/system_receiver.go
@@ -1,29 +1,42 @@
 package main
 
 import (
+	"github.com/filecoin-project/specs-actors/actors/abi/big"
+
 	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/specs-actors/actors/abi"
 	"github.com/filecoin-project/specs-actors/actors/runtime/exitcode"
 
 	. "github.com/filecoin-project/test-vectors/gen/builders"
 )
 
+// TODO: These tests may break in the future if sending to a system actor
+// becomes disallowed: https://github.com/filecoin-project/specs/issues/1069
 func transferToSystemActor(sysAddr address.Address) func(v *Builder) {
 	return func(v *Builder) {
 		v.Messages.SetDefaults(GasLimit(1_000_000_000), GasPremium(1), GasFeeCap(200))
+
+		initial := abi.NewTokenAmount(1_000_000_000_000)
+		transfer := abi.NewTokenAmount(10)
+
+		sysActor, err := v.StateTree.GetActor(sysAddr)
+		v.Assert.NoError(err, "failed to fetch actor %s from state", sysAddr)
+
+		// Calculate the end balance.
+		endBal := big.Add(sysActor.Balance, transfer)
 
 		// Set up sender account.
 		sender := v.Actors.Account(address.SECP256K1, initial)
 		v.CommitPreconditions()
 
-		// perform the transfer.
-		v.Messages.Sugar().Transfer(sender.Robust, sysAddr, transfer, Nonce(0))
+		v.Messages.Sugar().Transfer(sender.Robust, sysAddr, Value(transfer), Nonce(0))
 		v.CommitApplies()
 
-		v.Assert.BalanceEq(sender.Robust, initial)
-		v.Assert.Equal(v.PreRoot.String(), v.PostRoot.String(), "expected pre and post state root to be equal")
-
-		// TODO: These tests may break in the future if sending to a system actor
-		// becomes disallowed: https://github.com/filecoin-project/specs/issues/1069
+		// System actor received the funds.
+		v.Assert.BalanceEq(sysAddr, endBal)
+		// Sender sent the funds + gas.
+		v.Assert.EveryMessageSenderSatisfies(BalanceUpdated(transfer))
+		// Everything is great.
 		v.Assert.EveryMessageResultSatisfies(ExitCode(exitcode.Ok))
 	}
 }
