ff8da19493687f94bec79caefd245ffdbe8d62fb
Raúl Kripalani <raul@protocol.ai>
2020-09-08 11:24:58 +0100

    go fmt; point to a lotus commit contained in master; upgrade specs-actors (#130)

--- gen/suites/actor_creation/params.go
+++ gen/suites/actor_creation/params.go
@@ -4,10 +4,10 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/exitcode"
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	init_ "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	"github.com/filecoin-project/specs-actors/actors/builtin/paych"
-	"github.com/filecoin-project/go-state-types/exitcode"
 
 	. "github.com/filecoin-project/test-vectors/gen/builders"
 )
--- gen/suites/paych/ok.go
+++ gen/suites/paych/ok.go
@@ -4,10 +4,10 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-	init_ "github.com/filecoin-project/specs-actors/actors/builtin/init"
-	"github.com/filecoin-project/specs-actors/actors/builtin/paych"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/go-state-types/exitcode"
+	init_ "github.com/filecoin-project/specs-actors/actors/builtin/init"
+	"github.com/filecoin-project/specs-actors/actors/builtin/paych"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 
 	. "github.com/filecoin-project/test-vectors/gen/builders"
