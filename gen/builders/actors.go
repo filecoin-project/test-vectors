32ed9719c19989c472595fec48d41d4e6221dcf5
Alan Shaw <alan.shaw@protocol.ai>
2020-09-15 15:58:33 +0100

    fix: actors fallout

--- gen/builders/actors.go
+++ gen/builders/actors.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/account"
 	"github.com/filecoin-project/specs-actors/actors/builtin/miner"
