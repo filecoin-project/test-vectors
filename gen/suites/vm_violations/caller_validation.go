9db1dd8c9f85bd80eb624e77130a02a3aaf3a1f9
Alan Shaw <alan.shaw@protocol.ai>
2020-09-21 11:49:24 +0100

    test: additional caller validation tests (#146)
    A cut down version of https://github.com/filecoin-project/test-vectors/pull/145 with just additional tests for caller validation with addresses and type CIDs.
    
    Depends on:
    
    * [ ] https://github.com/filecoin-project/lotus/pull/3860

--- gen/suites/vm_violations/caller_validation.go
+++ gen/suites/vm_violations/caller_validation.go
@@ -5,21 +5,18 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/exitcode"
-	typegen "github.com/whyrusleeping/cbor-gen"
-
 	"github.com/filecoin-project/lotus/conformance/chaos"
 	. "github.com/filecoin-project/test-vectors/gen/builders"
 )
 
-func callerValidation(branch chaos.CallerValidationBranch, expectedCode exitcode.ExitCode) func(v *MessageVectorBuilder) {
+func callerValidation(args chaos.CallerValidationArgs, expectedCode exitcode.ExitCode) func(v *MessageVectorBuilder) {
 	return func(v *MessageVectorBuilder) {
 		v.Messages.SetDefaults(GasLimit(1_000_000_000), GasPremium(1), GasFeeCap(200))
 
 		alice := v.Actors.Account(address.SECP256K1, abi.NewTokenAmount(1_000_000_000_000))
 		v.CommitPreconditions()
 
-		cborBranch := typegen.CborInt(branch)
-		v.Messages.Raw(alice.ID, chaos.Address, chaos.MethodCallerValidation, MustSerialize(&cborBranch), Nonce(0), Value(big.Zero()))
+		v.Messages.Raw(alice.ID, chaos.Address, chaos.MethodCallerValidation, MustSerialize(&args), Nonce(0), Value(big.Zero()))
 		v.CommitApplies()
 
 		v.Assert.EveryMessageResultSatisfies(ExitCode(expectedCode))
